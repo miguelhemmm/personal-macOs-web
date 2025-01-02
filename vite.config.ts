19c491a07b8f55e099f200cf3d2bf6df69d8ca8a
Miguel <miguelhem@gmail.com>
2025-01-02 15:46:45 -0600

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/personal-macOs-web/",
   resolve: {
     alias: {
       i18n: "/src/i18n",
